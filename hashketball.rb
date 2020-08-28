7e5b9c451f559fcb4da0fda26f2058181ebdbf7d
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-28 03:10:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,15 +15,16 @@ def game_hash
           blocks: 1,
           slam_dunks: 1
         },
-        {player_name: "Reggie Evans",
-        number: 30,
-        shoe: 14,
-        points: 12,
-        rebounds: 12,
-        assists: 12,
-        steals: 12,
-        blocks: 12,
-        slam_dunks: 7
+        {
+          player_name: "Reggie Evans",
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
         },
         {player_name: "Brook Lopez",
         number: 11,
