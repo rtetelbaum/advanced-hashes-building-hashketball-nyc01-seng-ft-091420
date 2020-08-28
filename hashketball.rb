3d7d642aa4ea566c3d3086d4416ce2abeb8e0850
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-28 03:09:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,8 @@ def game_hash
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
       players: [
-        {player_name: "Alan Anderson",
+        {
+          player_name: "Alan Anderson",
         number: 0,
         shoe: 16,
         points: 22,
